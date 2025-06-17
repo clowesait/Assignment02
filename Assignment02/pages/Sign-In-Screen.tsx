4732e82a8be24f0c5cd285dbe116d6b8d3d5ba5c
clowesait <cody.lowe@edu.sait.ca>
2025-06-16 18:52:56 -0600

    Initial commit

---
+++ Assignment02/pages/Sign-In-Screen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SignInScreen() {
+
+  return (
+    <div>
+      <h1>Sign In</h1>
+      {/* Sign-in form will go here */}
+    </div>
+  );
+}
